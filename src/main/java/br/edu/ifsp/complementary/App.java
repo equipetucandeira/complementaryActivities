fcd662aa35096f7f30db1fabcd974a49585a0d85
Samuel do Prado Rodrigues <samuelprado730@gmail.com>
2024-08-10 18:06:29 -0300

    feat: app class

---
+++ src/main/java/br/edu/ifsp/complementary/App.java
@@ -0,0 +1,37 @@
+package br.edu.ifsp.complementary;
+
+import br.edu.ifsp.complementary.infrastructure.Configuration;
+import br.edu.ifsp.complementary.ui.SwingUI;
+
+import java.awt.Dimension;
+import java.awt.Rectangle;
+
+import javax.swing.SwingUtilities;
+
+public final class App {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					var configuration = new Configuration("application.properties");
+
+					var frame = SwingUI.getJFrame(
+						configuration.getProperty("app.window.title"),
+						configuration.getProperty("app.window.icon"),
+						null,
+						new Dimension(
+							Integer.valueOf(configuration.getProperty("app.window.width")),
+							Integer.valueOf(configuration.getProperty("app.window.height"))
+						),
+						SwingUI.getColor(configuration.getProperty("app.theme.primary"))
+					);
+
+					frame.setVisible(true);
+				} catch (Exception exception) {
+					exception.printStackTrace();
+				}
+			}
+		});
+	}
+}
