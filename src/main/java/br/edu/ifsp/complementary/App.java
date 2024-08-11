735c9cdd26ed7e86723a3d5bde166b555b28f06d
Samuel do Prado Rodrigues <samuelprado730@gmail.com>
2024-08-11 15:47:16 -0300

    refactor: add app configuration class + exception handling

--- src/main/java/br/edu/ifsp/complementary/App.java
+++ src/main/java/br/edu/ifsp/complementary/App.java
@@ -1,59 +1,55 @@
 package br.edu.ifsp.complementary;
 
-import br.edu.ifsp.complementary.infrastructure.Configuration;
-import br.edu.ifsp.complementary.infrastructure.ConfigurationException;
+import br.edu.ifsp.complementary.data.Jdbc;
 import br.edu.ifsp.complementary.ui.SwingUI;
 
 import java.awt.Dimension;
-import java.awt.Rectangle;
 
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
 public final class App {
-	private static String title;
+	private static AppConfiguration configuration;
 
-	private static Dimension dimensions;
+	private static Jdbc dataSource;
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
 				try {
-					var configuration = new Configuration("application.properties");
+					configuration = new AppConfiguration("application.properties");
 
-					App.title = configuration.getProperty("app.window.title");
+					Class.forName(configuration.getDatabaseDriver());
 
-					App.dimensions = new Dimension(
-						Integer.valueOf(configuration.getProperty("app.window.width")),
-						Integer.valueOf(configuration.getProperty("app.window.height"))
+					dataSource = new Jdbc(
+						configuration.getDatabaseURL(),
+						configuration.getDatabaseUser(),
+						configuration.getDatabasePassword()
 					);
-
-					try {
-						Class.forName("db.driver");
-					} catch (ClassNotFoundException exception) {
-						showErrorMessage("Driver " + configuration.getProperty("db.driver") + " não encontrado.");
-
-						return;
-					}
-
-					var frame = SwingUI.getJFrame(
-						App.title,
-						configuration.getProperty("app.window.icon"),
-						null,
-						App.dimensions,
-						SwingUI.getColor(configuration.getProperty("app.theme.primary"))
-					);
-
-					frame.setVisible(true);
-				} catch (ConfigurationException exception) {
+				} catch (ClassNotFoundException exception) {
+					showErrorMessage("Driver " + configuration.getDatabaseDriver() + " não encontrado.");
+				} catch (Exception exception) {
 					showErrorMessage(exception.getMessage());
 				}
+
+				var frame = SwingUI.getJFrame(
+					configuration.getTitle(),
+					configuration.getIcon(),
+					null,
+					new Dimension(
+						configuration.getWidth(),
+						configuration.getHeight()
+					),
+					SwingUI.getColor(configuration.getBackgroundColor())
+				);
+
+				frame.setVisible(true);
 			}
 		});
 	}
 
 	private static void showErrorMessage(String message) {
-		JOptionPane.showMessageDialog(null, message, App.title, JOptionPane.ERROR_MESSAGE);
+		JOptionPane.showMessageDialog(null, message, configuration.getTitle(), JOptionPane.ERROR_MESSAGE);
 	}
 }
