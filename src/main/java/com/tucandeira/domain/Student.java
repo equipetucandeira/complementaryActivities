5ffa2b20e4fccb4f14139879feae239238539575
Samuel do Prado Rodrigues <samuelprado730@gmail.com>
2024-08-14 16:29:53 -0300

    feat: student

---
+++ src/main/java/com/tucandeira/domain/Student.java
@@ -0,0 +1,61 @@
+package com.tucandeira.domain;
+
+import com.tucandeira.domain.Email;
+import com.tucandeira.domain.Password;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+@SuppressWarnings("serial")
+public final class Student implements Serializable {
+  private UUID uuid;
+
+  private String name;
+
+  private Email email;
+
+  private Password password;
+
+  public Student(String name, Email email, Password password) {
+    this.uuid = UUID.randomUUID();
+
+    this.name = name;
+
+    this.email = email;
+  }
+
+  public Student(UUID id, String name, Email email, Password password) {
+    this.uuid = id;
+
+    this.name = name;
+
+    this.email = email;
+  }
+
+  public UUID getUUID() {
+    return this.uuid;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public Email getEmail() {
+    return this.email;
+  }
+
+  public Password getPassword() {
+    return this.password;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(uuid, name, email);
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    return getClass() == object.getClass() || hashCode() == object.hashCode();
+  }
+}
