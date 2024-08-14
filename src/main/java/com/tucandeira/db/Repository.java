9f8165d725db5defaafa70156734190f4315efa1
Samuel do Prado Rodrigues <samuelprado730@gmail.com>
2024-08-14 19:23:10 -0300

    refactor: methods signatures

--- src/main/java/com/tucandeira/db/Repository.java
+++ src/main/java/com/tucandeira/db/Repository.java
@@ -4,14 +4,14 @@ import java.util.Collection;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface Repository<T> {
-  T save(T object);
+public interface Repository<T> extends ResultSetTypecasting<T> {
+  boolean save(T object);
 
   Collection<T> list();
 
   Optional<T> find(UUID uuid);
 
-  T update(T object);
+  boolean update(T object);
 
-  boolean delete(T object);
+  boolean delete(UUID uuid);
 }
