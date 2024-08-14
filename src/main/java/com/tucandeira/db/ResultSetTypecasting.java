c188966b8c76433917b601afa0b79456c7f975e3
Samuel do Prado Rodrigues <samuelprado730@gmail.com>
2024-08-14 15:23:01 -0300

    feat: result set typecasting interface

---
+++ src/main/java/com/tucandeira/db/ResultSetTypecasting.java
@@ -0,0 +1,8 @@
+package com.tucandeira.db;
+
+import com.tucandeira.common.Typecasting;
+
+import java.sql.ResultSet;
+
+public interface ResultSetTypecasting<T> extends Typecasting<ResultSet,T> {
+}
