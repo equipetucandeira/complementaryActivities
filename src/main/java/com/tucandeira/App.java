5d1825d5c6845171232383dff2c1e6ee0c666d00
Samuel do Prado Rodrigues <samuelprado730@gmail.com>
2024-08-18 18:22:41 -0300

    refactor: clean up app class

--- src/main/java/com/tucandeira/App.java
+++ src/main/java/com/tucandeira/App.java
@@ -2,7 +2,6 @@ package com.tucandeira;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,17 +10,9 @@ import java.util.logging.Logger;
 import java.util.logging.Level;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
+import javafx.scene.*;
 import javafx.stage.*;
-import javafx.scene.Parent;
-
-import com.tucandeira.ui.JavaFX;
 
 public final class App extends Application {
   private static Connection connection;
@@ -35,10 +26,6 @@ public final class App extends Application {
   @Override
   public void start(Stage stage) {
     try { 
-      var loader = new FXMLLoader(new File("src/main/java/com/tucandeira/ui/menuScreen.fxml").toURI().toURL());
-
-      Parent root = loader.load();
-      
       var resourcePath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
 
       var stream = new FileInputStream(resourcePath + "/application.properties");
@@ -57,8 +44,6 @@ public final class App extends Application {
         properties.getProperty("db.password")
       );
 
-      var mainMenu = createMainMenuScene(stage);
-
       stage.setTitle(properties.getProperty("app.window.title"));
 
       stage.initStyle(StageStyle.UNDECORATED);
@@ -71,10 +56,13 @@ public final class App extends Application {
 
       stage.setY((bounds.getHeight() - stage.getHeight()) / 2);
 
-      //setupScene(stage, mainMenu);
-      
-      var scene = new Scene(root);
-      setupScene(stage, scene);
+      var fxml = "src/main/java/com/tucandeira/ui/menuScreen.fxml";
+
+      Parent root = new FXMLLoader(new File(fxml).toURI().toURL()).load();
+
+      stage.setScene(new Scene(root));
+
+      stage.centerOnScreen();
 
       stage.show();
     } catch (Exception exception) {
@@ -87,236 +75,5 @@ public final class App extends Application {
   @Override
   public void stop() {
     System.exit(0);
-  }
-
-  public void setupScene(Stage stage, Scene scene) {
-    stage.setScene(scene);
-    stage.centerOnScreen();
-  }
-
-  private static Scene getScene(GridPane grid) {
-    return new Scene(grid, Integer.valueOf(properties.getProperty("app.window.width")), Integer.valueOf(properties.getProperty("app.window.height")));
-  }
-
-  private static Scene getScene(VBox box) {
-    return new Scene(box, Integer.valueOf(properties.getProperty("app.window.width")), Integer.valueOf(properties.getProperty("app.window.height")));
-  }
-
-  private Scene createMainMenuScene(Stage stage) {
-    var label = new Label(properties.getProperty("app.window.title"));
-
-    var email = new TextField();
-
-    var password = new PasswordField();
-        
-    var goToSceneStudent = new Button("Adicionar atividade");
-    goToSceneStudent.setOnAction(event -> stage.setScene(createSceneStudent(stage)));
-        
-    var goToSceneEvaluator = new Button("Avaliar atividade");
-    goToSceneEvaluator.setOnAction(event -> stage.setScene(createSceneEvaluator(stage)));
-
-    var goToSceneList = new Button("Listar atividades");
-    goToSceneList.setOnAction(event -> stage.setScene(createSceneListActivities(stage)));
-
-    var close = new Button("Sair");
-    close.setOnAction(event -> stage.close());
-    
-    var layout = new VBox(15);
-    layout.setAlignment(Pos.CENTER);
-    layout.setPadding(new Insets(20));
-    layout.getChildren().addAll(label, email, password, goToSceneStudent, goToSceneEvaluator, goToSceneList, close);
-        
-    return getScene(layout);
-  }
-
-  private Scene createSceneStudent(Stage stage) {
-    var typeAndPeriodLabel = new Label("Tipo e Período Letivo");
-
-    var label = new Label("Adicionar atividade");
-    var backToMenu = new Button("Voltar para Menu Principal");
-    backToMenu.setOnAction(event -> stage.setScene(createMainMenuScene(stage)));
-
-    var vinculationLabel = new Label("Vinculação:");
-        
-    var option1 = new RadioButton("Curricular");
-    var option2 = new RadioButton("Não Curricular");
-    var group = new ToggleGroup();
-    option1.setToggleGroup(group);
-    option2.setToggleGroup(group);
-
-    var activityTypeLabel = new Label("Tipo:");
-    var comboBox = new ComboBox<String>();
-    comboBox.getItems().addAll("Opção 1", "Opção 2", "Opção 3");
-
-    var activityNameLabel = new Label("Nome da Atividade:");
-    var activityNameField = new TextField();
-
-    var workloadLabel = new Label("Carga Horária:");
-    var workloadField = new TextField();
-
-    var startDateLabel = new Label("Data de Início:");
-    var startDatePicker = new DatePicker();
-
-    var endDateLabel = new Label("Data de Término:");
-    var endDatePicker = new DatePicker();
-
-    var attachmentLabel = new Label("Anexo:");
-    var attachmentButton = new Button("Escolher Arquivo");
-    attachmentButton.setOnAction(event -> {
-      var fileChooser = new FileChooser();
-
-      fileChooser.setTitle("Selecione um documento");
-
-      fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("PDF", "*.pdf"));
-
-      var selectedFile = fileChooser.showOpenDialog(stage);
-
-      if (selectedFile != null) {
-        attachmentButton.setText(selectedFile.getName());
-      } else {
-        System.out.println("Erro ao ler arquivo");
-      }
-    });
-
-    var saveButton = new Button("Salvar");
-    var resultLabel = new Label();
-
-    var grid = new GridPane();
-    grid.setHgap(10);
-    grid.setVgap(10);
-    grid.setPadding(new Insets(10));
-    grid.setAlignment(Pos.CENTER);
-        
-    grid.add(vinculationLabel, 0, 2);
-    grid.add(option1, 1, 2);
-    grid.add(option2, 2, 2);
-
-    grid.add(activityTypeLabel, 0, 3);
-    grid.add(comboBox, 1, 3);
-
-    grid.add(activityNameLabel, 0, 4);
-    grid.add(activityNameField, 1, 4);
-
-    grid.add(workloadLabel, 0, 5);
-    grid.add(workloadField, 1, 5);
-
-    grid.add(startDateLabel, 0, 6);
-    grid.add(startDatePicker, 1, 6);
-
-    grid.add(endDateLabel, 0, 7);
-    grid.add(endDatePicker, 1, 7);
-
-    grid.add(attachmentLabel, 0, 9);
-    grid.add(attachmentButton, 1, 9);
-
-    grid.add(saveButton, 1, 10);
-    grid.add(resultLabel, 1, 11);
-
-    grid.add(backToMenu, 0, 12, 2, 1);
-
-    return getScene(grid);
-  }
-
-  private Scene createSceneEvaluator(Stage stage) {    
-    var titleLabel = new Label("Título da Tarefa:");
-    var taskTitle = new Label("Atividade Exemplo");
-        
-    var typeLabel = new Label("Tipo da Tarefa:");
-    var taskType = new Label("Tipo Exemplo");
-
-    var approveButton = new Button("Aprovar");
-    var rejectButton = new Button("Reprovar");
-    var detailsButton = new Button("Ver Detalhes");
-
-    detailsButton.setOnAction(event -> stage.setScene(createSceneDetails(stage)));
-        
-    var backToMenu = new Button("Voltar para Menu Principal");
-    backToMenu.setOnAction(event -> stage.setScene(createMainMenuScene(stage)));
-
-    var grid = new GridPane();
-    grid.setHgap(10);
-    grid.setVgap(10);
-    grid.setPadding(new Insets(10));
-    grid.setAlignment(Pos.CENTER);
-        
-    grid.add(titleLabel, 0, 0);
-    grid.add(taskTitle, 1, 0);
-    grid.add(typeLabel, 0, 1);
-    grid.add(taskType, 1, 1);
-
-    grid.add(approveButton, 0, 2);
-    grid.add(rejectButton, 1, 2);
-    grid.add(detailsButton, 2, 2);
-
-    grid.add(backToMenu, 0, 3, 3, 1);
-
-    return getScene(grid);
-  }
-
-  private Scene createSceneListActivities(Stage stage) {
-    var label = new Label("Lista de atividades");
-    var listView = new ListView<String>();
-    var activities = FXCollections.observableArrayList(
-      "Exemplo 1 - Tipo: Curricular, Data: 01/01/2024",
-      "Exemplo 2 - Tipo: Não Curricular, Data: 15/01/2024",
-      "Exemplo 3 - Tipo: Curricular, Data: 30/01/2024"
-    );
-    listView.setItems(activities);
-    listView.setPrefWidth(Integer.valueOf(properties.getProperty("app.window.width")));
-    listView.setPrefHeight(Integer.valueOf(properties.getProperty("app.window.height")));
-
-    var backToMenu = new Button("Voltar para Menu Principal");
-    backToMenu.setOnAction(event -> stage.setScene(createMainMenuScene(stage)));
-        
-    var grid = new GridPane();
-    grid.setHgap(10);
-    grid.setVgap(10);
-
-    grid.setPadding(new Insets(10));
-    grid.setAlignment(Pos.CENTER);
-    
-    grid.add(label, 0, 0, 2, 1); 
-    grid.add(listView, 0, 1, 2, 1); 
-    grid.add(backToMenu, 0, 2); 
-        
-    return getScene(grid);
-  }
-
-  private Scene createSceneDetails(Stage stage){
-    var titleLabel = new Label("Detalhes da Atividade");
-
-    var vinculationLabel = new Label("Vinculação:");
-    var vinculationValue = new Label("Curricular");
-
-    var typeLabel = new Label("Tipo:");
-    var typeValue = new Label("Tipo selecionado");
-
-    var additionalInfoLabel = new Label("Informações Complementares:");
-    var additionalInfoArea = new TextArea("");
-    additionalInfoArea.setEditable(false);
-
-    var backButton = new Button("Voltar");
-    backButton.setOnAction(event -> stage.setScene(createSceneEvaluator(stage)));
-
-    var grid = new GridPane();
-    grid.setHgap(10);
-    grid.setVgap(10);
-    grid.setPadding(new Insets(10));
-    grid.setAlignment(Pos.CENTER);
-
-    grid.add(titleLabel, 0, 0, 2, 1);
-
-    grid.add(vinculationLabel, 0, 3);
-    grid.add(vinculationValue, 1, 3);
-
-    grid.add(typeLabel, 0, 4);
-    grid.add(typeValue, 1, 4);
-
-    grid.add(additionalInfoLabel, 0, 5);
-    grid.add(additionalInfoArea, 0, 6, 2, 1);
-
-    grid.add(backButton, 0, 7, 2, 1);
-
-    return getScene(grid);
   }
 }
